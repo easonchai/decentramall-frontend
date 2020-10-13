527776c5ae6e77c1c2935e2ba01d7886bf10fb0a
Eason Chai <echai2905@gmail.com>
2020-10-13 20:35:05 +0800

    Added web3

---
+++ src/components/ConnectWallet.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from "@material-ui/core";
+
+export default function ConnectWallet() {
+    return(
+        <Button>
+            Connect Wallet
+        </Button>
+    )
+}
---
+++ src/services/ModalService.tsx
@@ -0,0 +1,7 @@
+import Web3 from "web3";
+import Web3Modal from "web3modal";
+
+export default class ModalService{
+    
+}
+
