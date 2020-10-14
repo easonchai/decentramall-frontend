9d294ba9099e0e90534594b5f0068d871b2ecb36
Eason Chai <echai2905@gmail.com>
2020-10-15 00:09:35 +0800

    Added prototype new user page

--- src/components/Space.tsx
+++ src/components/Space.tsx
@@ -1,18 +1,48 @@
-import { makeStyles } from '@material-ui/core';
+import { Button, makeStyles, Theme, Typography } from '@material-ui/core';
 import React from 'react';
 import Graph from './Graph';
 import Header from './Header';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => ({
   root: {
     width: '85%', 
     margin: 'auto', 
   },
   graph: {
     width: '50%',
-    margin: 'auto'
+    margin: 'auto',
+    marginTop: '2rem'
+  },
+  heading: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: theme.palette.secondary.main,
+    fontSize: '2rem',
+    marginTop: '3rem'
+  },
+  newUserOptions: {
+    // #429AFF
+    display: 'flex',
+    width: '100%',
+    margin: 'auto',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '2rem'
+  },
+  primaryButton: {
+    borderRadius: '100px',
+    fontSize: '3rem',
+    color: 'white',
+    padding: '1rem 2rem',
+    margin: '1rem'
+  },
+  secondaryButton: {
+    borderRadius: '100px',
+    fontSize: '3rem',
+    margin: '1rem',
+    padding: '1rem 2rem'
   }
-});
+}));
 
 export default function Space() {
   const classes = useStyles();
@@ -20,10 +50,18 @@ export default function Space() {
   return (
     <div className={classes.root}>
         <Header />
+        <Typography component="h1" className={classes.heading}>Current SPACE Price</Typography>
         <div className={classes.graph}>
           <Graph />
         </div>
-        
+        <span className={classes.newUserOptions}>
+          <Button variant="contained" color="primary" className={classes.primaryButton}>
+            Buy Space
+          </Button>
+          <Button variant="outlined" color="primary" className={classes.secondaryButton}>
+            Rent Space
+          </Button>
+        </span>
     </div>
   );
 }
