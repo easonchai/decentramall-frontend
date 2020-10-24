09d89788b6ea4f62616f0eb5c84cf8bdfa9b708f
Eason Chai <echai2905@gmail.com>
2020-10-24 12:11:14 +0800

    slight fix to connect

--- src/services/ConnectWallet.tsx
+++ src/services/ConnectWallet.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import Web3Connect from "web3connect";
 import WalletConnectProvider from "@walletconnect/web3-provider";
-import Portis from "@portis/web3";
-import Fortmatic from "fortmatic";
+// import Portis from "@portis/web3";
+// import Fortmatic from "fortmatic";
+// import MewConnect from "@myetherwallet/mewconnect-web-client";
 import Torus from "@toruslabs/torus-embed";
-import MewConnect from "@myetherwallet/mewconnect-web-client";
 import Web3 from 'web3';
 
 export default function ConnectWallet(){
   return (
     <Web3Connect.Button
-      network="mainnet" // optional
+      network="ropsten" // optional
       providerOptions={{
         walletconnect: {
           package: WalletConnectProvider, // required
           options: {
-            infuraId: "INFURA_ID" // required
+            infuraId: "0517ded556c34f80b1c8919fe712a995" // required
           }
         },
-        portis: {
-          package: Portis, // required
-          options: {
-            id: "PORTIS_ID" // required
-          }
-        },
+        // portis: {
+        //   package: Portis, // required
+        //   options: {
+        //     id: "PORTIS_ID" // required
+        //   }
+        // },
         torus: {
           package: Torus, // required
           options: {
@@ -33,22 +33,23 @@ export default function ConnectWallet(){
             showTorusButton: true // optional
           }
         },
-        fortmatic: {
-          package: Fortmatic, // required
-          options: {
-            key: "FORTMATIC_KEY" // required
-          }
-        },
-        mewconnect: {
-          package: MewConnect, // required
-          options: {
-            infuraId: "INFURA_ID" // required
-          }
-        }
+        // fortmatic: {
+        //   package: Fortmatic, // required
+        //   options: {
+        //     key: "FORTMATIC_KEY" // required
+        //   }
+        // },
+        // mewconnect: {
+        //   package: MewConnect, // required
+        //   options: {
+        //     infuraId: "" // required
+        //   }
+        // }
       }}
       onConnect={(provider: any) => {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const web3 = new Web3(provider); // add provider to web3
+        console.log("connected!")
       }}
       onClose={() => {
         console.log("Web3Connect Modal Closed"); // modal has closed
