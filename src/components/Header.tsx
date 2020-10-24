388e70adbc2b49cfffccd19143716cc0a665bc45
Eason Chai <echai2905@gmail.com>
2020-10-24 12:19:57 +0800

    remove connectwallet

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,13 @@
 import { Link, makeStyles, Theme, Typography } from '@material-ui/core';
-import React from 'react';
-import ConnectWallet from '../services/ConnectWallet';
+import React, { useState } from 'react';
+import Blockies from 'react-blockies';
+import Web3Connect from "web3connect";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+// import Portis from "@portis/web3";
+// import Fortmatic from "fortmatic";
+// import MewConnect from "@myetherwallet/mewconnect-web-client";
+import Torus from "@toruslabs/torus-embed";
+import Web3 from 'web3';
 
 const useStyles = makeStyles((theme: Theme) => ({
     logo: {
@@ -41,6 +48,7 @@ const useStyles = makeStyles((theme: Theme) => ({
 
 export default function Header(){
     const classes = useStyles();
+    const [connected, setConnected] = useState(false);
 
     const isActive = (href:string) => {
         if (href === window.location.pathname){
@@ -60,7 +68,57 @@ export default function Header(){
                 <Link href="/space" className={isActive("/space") ? classes.active : classes.link} underline="none">Space</Link>
             </ul>
             <div className={classes.connect}>
-                <ConnectWallet />
+                {
+                connected ?
+                    "Connected"
+                    :
+                    <Web3Connect.Button
+                        network="ropsten" // optional
+                        providerOptions={{
+                            walletconnect: {
+                            package: WalletConnectProvider, // required
+                            options: {
+                                infuraId: "0517ded556c34f80b1c8919fe712a995" // required
+                            }
+                            },
+                            // portis: {
+                            //   package: Portis, // required
+                            //   options: {
+                            //     id: "PORTIS_ID" // required
+                            //   }
+                            // },
+                            torus: {
+                            package: Torus, // required
+                            options: {
+                                enableLogging: false, // optional
+                                buttonPosition: "bottom-left", // optional
+                                buildEnv: "production", // optional
+                                showTorusButton: true // optional
+                            }
+                            },
+                            // fortmatic: {
+                            //   package: Fortmatic, // required
+                            //   options: {
+                            //     key: "FORTMATIC_KEY" // required
+                            //   }
+                            // },
+                            // mewconnect: {
+                            //   package: MewConnect, // required
+                            //   options: {
+                            //     infuraId: "" // required
+                            //   }
+                            // }
+                        }}
+                        onConnect={(provider: any) => {
+                            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                            const web3 = new Web3(provider); // add provider to web3
+                            console.log("connected!")
+                        }}
+                        onClose={() => {
+                            console.log("Web3Connect Modal Closed"); // modal has closed
+                        }}
+                        />
+                }
             </div>
         </nav>
     )
